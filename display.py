8b101356252e98e7e750dd337a69a3c8e5fda440
Paul van Duyn <paul.vanduyn@gmail.com>
2018-06-14 18:52:41 -0400

    some code is in display now

---
+++ display.py
@@ -0,0 +1,18 @@
+
+
+def displayBoard(gameState):
+
+#    a   b   c
+# 1  o |   |
+#   -----------
+# 2    | x |
+#   -----------
+# 3    |   |
+
+    # first line is always the same
+    print '   a   b   c'
+
+    for line in gameState:
+		print '    %s' % worker.name
+
+		print ''
