e9831d20138222fcfca7bce819b9d8773a4e57ec
Ryan Lambert <ryan.g.lambert@gmail.com>
2018-06-14 19:16:06 -0400

    moved a few things

--- main.py
+++ main.py
@@ -1,5 +1,23 @@
 import argparse
-parser = argparse.ArgumentParser()
-parser.add_argument("x")
-args = parser.parse_args()
-print (args)
+
+
+class TicTacToe(object):
+    def __init__(self, grid_size):
+	self.board = self.make_board()
+
+
+    def make_board(self, grid_size):
+	for x in range(grid_size):
+            board.append(["O"] * grid_size)
+	return board
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("x")
+    args = parser.parse_args()
+    print (args)
+
+
+if __name__ == '__main__':
+    main():
