c3ea4aeae630e3693a52ac0e825b7ad39d092642
Ryan Lambert <ryan.g.lambert@gmail.com>
2018-06-14 20:06:42 -0400

    latest

--- main.py
+++ main.py
@@ -1,19 +1,28 @@
 import argparse
 import display
+
 from game import TicTacToe
 
 
 def main():
-    # parse stuff here:
-    # give parsed stuff to the class
-    game = TicTacToe()
-    x_move = True
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-o", help="User plays as the Os", action='store_true')
+    parser.add_argument("-x", help="User plays as the Xs", action='store_true')
+    args = parser.parse_args()
+    player = ''
+    if args.o:
+       player = 'o'
+    elif args.x:
+       player = 'x'
+
+    print("Playing with: ", player)
+    game = TicTacToe(3, player)
     while True:
-        user_selection = raw_input()
-        game.update_board()
-    # play the game maybe a while loop
-    # during each while use display.py to update what is shown to the user
-        x_move = not True
+        display.displayBoard(game.board)
+        col, row = input('Please choose a move: (i.e. \'00\')')
+        col, row = int(col), int(row)
+        game.update_board((col, row), player)
+        # opponent's move
 
 
 if __name__ == '__main__':
